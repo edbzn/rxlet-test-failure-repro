13d97e4a902d19f4dcc8e2492c3f6d8ecc803979
Edouard Bozon <bozonedouard@gmail.com>
2021-05-04 14:54:13 +0200

    setting up jest

--- apps/repro/src/app/app.component.spec.ts
+++ apps/repro/src/app/app.component.spec.ts
@@ -1,31 +1,24 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { LetModule, PushModule } from '@rx-angular/template';
+
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [LetModule, PushModule],
       declarations: [AppComponent],
     }).compileComponents();
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'repro'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('repro');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  it('render "hello" text', () => {
+    fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain(
-      'Welcome to repro!'
-    );
+    expect(
+      fixture.debugElement.query(By.css('div')).nativeElement.innerHTML
+    ).toContain('hello');
   });
 });
--- apps/repro/src/app/app.component.ts
+++ apps/repro/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { of } from 'rxjs';
 
 @Component({
-  selector: 'rxlet-failing-test-repro-root',
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'repro';
+  state$ = of('hello');
 }
